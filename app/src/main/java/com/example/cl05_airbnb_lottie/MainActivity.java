1c0a81fb1f11d4a3292f7fb029e550ee9120ae90
José Francisco Jarquin Martínez <ex500791.jose.jarquin@mail.telcel.com>
2022-12-07 14:06:08 -0600

    la aplicación recién creada

---
+++ app/src/main/java/com/example/cl05_airbnb_lottie/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cl05_airbnb_lottie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
